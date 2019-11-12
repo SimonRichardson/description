0500307298cdfc208a791d2b94a05d0b53be88d1
Simon Richardson <stickupkid@gmail.com>
2019-11-12 16:14:13 +0000

    Status Omit for remote applications
    The following attempts to allow optional values for status fields
    on remote applications. Some remote applications don't have status
    records, which is fine, but it means that we need to have a
    flexible schema to model that.

--- remoteapplication.go
+++ remoteapplication.go
@@ -220,7 +220,7 @@ func newRemoteApplicationFromValid(valid map[string]interface{}, version int) (*
 		IsConsumerProxy_: valid["is-consumer-proxy"].(bool),
 	}
 
-	if rawStatus := valid["status"]; rawStatus != nil {
+	if rawStatus, ok := valid["status"]; ok && rawStatus != nil {
 		status, err := importStatus(rawStatus.(map[string]interface{}))
 		if err != nil {
 			return nil, errors.Trace(err)
@@ -267,6 +267,7 @@ func remoteApplicationV1Fields() (schema.Fields, schema.Defaults) {
 		"endpoints":         schema.Omit,
 		"spaces":            schema.Omit,
 		"bindings":          schema.Omit,
+		"status":            schema.Omit,
 		"is-consumer-proxy": false,
 	}
 	return fields, defaults
